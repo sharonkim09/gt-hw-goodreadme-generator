a8d0f80ff2cb63f45059c4916eb82f0cb23c0398
Sharon Kim <sharonkim1709@gmail.com>
2020-06-25 14:58:20 -0400

    copied over develop folder

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+// require fs
+// require inquirer 
+
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+// use fs package
+}
+
+// function to initialize program
+function init() {
+// use inquirer package
+}
+
+// function call to initialize program
+init();
