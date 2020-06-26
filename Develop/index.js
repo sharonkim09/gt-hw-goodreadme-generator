4ada31ac70a71ff15673fd49e8fe9f9fcf066fcc
Sharon Kim <sharonkim1709@gmail.com>
2020-06-25 23:56:37 -0400

    added list type for user to choose from licenses

--- Develop/index.js
+++ Develop/index.js
@@ -27,9 +27,34 @@ const questions = [
     name: "description",
   },
   {
-    type: "input",
+    //   have to let user select several options
+    type: "list",
     message: "What kind of license should your project have?",
     name: "license",
+    choices: [
+      {
+        name: "MIT",
+        badge:
+          "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+      },
+      {
+          name: "APACHE 2.0",
+          badge:
+          "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      },
+      {
+          name: "GPL 3.0",
+          badge: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      },
+      {
+          name: "BSD 3",
+          badge: "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+      },
+      {
+          name: "None",
+          badge: "No License"
+      }
+    ],
   },
   {
     type: "input",
@@ -74,5 +99,5 @@ function init() {
 }
 // console.log(response)
 // function call to initialize program
-writeToFile();
+// writeToFile();
 init();
